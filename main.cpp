ae208f83bf587f7b3e3903189916f83770f46ffb
d000b <makc.hitman@gmail.com>
2024-02-24 02:24:49 +0800

    for_each update

--- main.cpp
+++ main.cpp
@@ -17,18 +17,20 @@
 #include <vector>
 #include <algorithm>
 #include <iterator>
+#include <utility>
+#include <execution>
 
 #include "particle.h"
 
 
-int main() 
+int main()
 {
     const int screenWidth = 800;
     const int screenHeight = 800;
 
     raylib::SetRandomSeed(1);
 
-    const int particleCount = 100000;
+    const int particleCount = 100'000;
     std::vector<Particle> particles;
     particles.reserve(particleCount);
 
@@ -47,19 +49,17 @@ int main()
         const auto force = static_cast<float>(raylib::IsMouseButtonDown(raylib::MOUSE_BUTTON_LEFT))
                          - static_cast<float>(raylib::IsMouseButtonDown(raylib::MOUSE_BUTTON_RIGHT));
 
-        for (auto& particle : particles)
-        {
+        std::for_each(std::execution::par_unseq, std::begin(particles), std::end(particles), [&](Particle& particle) {
             particle.updatePosition(mousePos, force, 0.99, screenWidth, screenHeight);
-        }
+        });
 
         raylib::BeginDrawing();
 
         raylib::ClearBackground(white_background);
 
-        for (auto& particle : particles)
-        {
+        std::for_each(std::execution::seq, std::begin(particles), std::end(particles), [&](Particle& particle) {
             particle.drawPixel();
-        }
+        });
 
         raylib::DrawFPS(10, 10);
 
