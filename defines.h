4133c90944546b6b77c3d3df7ee99ec248b57061
d000b <makc.hitman@gmail.com>
2024-02-27 22:00:53 +0800

    update to separate defines

---
+++ defines.h
@@ -0,0 +1,15 @@
+
+#define WIN32_LEAN_AND_MEAN
+#if defined(_WIN32)
+#define WIN32
+#endif
+#if defined(_WIN64)
+#define WIN64
+#define _AMD64_
+#undef _X86_
+#else
+#undef _AMD64_
+#define _X86_
+#endif
+
+#include <minwindef.h>
--- main.cpp
+++ main.cpp
@@ -1,18 +1,6 @@
+#pragma once
 
-#define WIN32_LEAN_AND_MEAN
-#if defined(_WIN32)
-#define WIN32
-#endif
-#if defined(_WIN64)
-#define WIN64
-#define _AMD64_
-#undef _X86_
-#else
-#undef _AMD64_
-#define _X86_
-#endif
-
-#include <minwindef.h>
+#include "defines.h"
 
 #include <vector>
 #include <algorithm>
